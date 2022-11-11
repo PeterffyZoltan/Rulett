8feb1d8ff66a700db779dd18c93f9df8bf16c7e1
Péterffy Zoltán <peterffy.zoltan@students.jedlik.eu>
2022-11-11 12:29:31 +0100

    animált kerék

--- kerek.js
+++ kerek.js
@@ -1,23 +1,67 @@
-let forgatas_merteke = 360/37;
-let kerek_helyzet = 0;
+let kerekHelyzet = 0;
+let labdaForgHelyzet = 0;
+let porgete = false;
+const kerek = document.getElementById('kerek');
+const labdaWrap = document.getElementById('labda-wrapper');
+const labda = document.getElementById('labda');
+let nyertesszam;
 
 function porget() {
 
-    const kerek = document.getElementById('kerek');
-    porgetes_merteke = Math.floor(Math.random()*(1080-720+1)+720)
-
+    if (porgete) {
+        return 0;
+    }
+    porgete = true
+    labdamegall = false
+    labdaHelyzet = 1;
+    osszPorgetes = Math.floor(Math.random()*(1080-720+1)+720)
+    
     forgatas = setInterval(() => {
-        let forgatas_merteke = (360/37/10) *( porgetes_merteke/360 *3);
-        kerek_helyzet += forgatas_merteke; 
-        porgetes_merteke -= forgatas_merteke;
-        if (kerek_helyzet> 360) {
-            kerek_helyzet = 0;
+        let forgatasMerteke = (360/37/10) *( osszPorgetes/360*3);
+        
+        kerekHelyzet += forgatasMerteke; 
+        if(labdaHelyzet < 20 ){
+            labdaForgHelyzet += -1*(forgatasMerteke);
+            labdaHelyzet += 0.1;
         }
-        kerek.style.rotate = String(kerek_helyzet)+ "deg";
-        console.log(kerek_helyzet)
-        if (porgetes_merteke < 20) {
+        else if(!labdamegall){
+            labdaHelyzet = 20;
+            labdamegall = true;
+            labdaMegakad();
+        }
+        else{
+            labdaHelyzet = 20;
+            labdaForgHelyzet += forgatasMerteke;
+        }
+
+        osszPorgetes -= forgatasMerteke;
+        if (kerekHelyzet> 360) {
+            kerekHelyzet = 0;
+        }
+        if (labdaForgHelyzet< 0) {
+            labdaForgHelyzet = 360;
+        }
+        if(labdaForgHelyzet > 360){
+            labdaForgHelyzet = 0;
+        }
+        kerek.style.rotate = String(kerekHelyzet)+ "deg";
+        labdaWrap.style.rotate = String(labdaForgHelyzet)+ "deg";
+        labda.style.top = String(labdaHelyzet) + "%";
+        if (osszPorgetes < 10) {
+            porgete = false;
             clearInterval(forgatas)
+
         }
     }, 1);    
  
 }
+
+function labdaMegakad(){
+    nyertesszam = Math.round( Math.abs( (labdaForgHelyzet))/ ( 360/37 )- Math.abs((kerekHelyzet / (360/37))))
+    nyertesszam += 37;
+    nyertesszam = nyertesszam%37;
+    console.log(nyertesszam)
+    labdaForgHelyzet = nyertesszam * (360/37) + kerekHelyzet;
+}
+
+
