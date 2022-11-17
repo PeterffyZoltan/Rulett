4df712cf5bd464a4bda1e0f24e7ca187154688e1
Sári András <sari.andras@students.jedlik.eu>
2022-11-17 19:00:44 +0100

    Bets, hovers done

--- table.js
+++ table.js
@@ -2,18 +2,27 @@ var bets = new Object();
 var bets = {};
 var bet = 500;
 var allMoney = 5000;
+var oneRound = 0;
 const elsoTizenketto = [1,2,3,4,5,6,7,8,9,10,11,12];
 const masodikTizenketto = [13,14,15,16,17,18,19,20,21,22,23,24];
 const harmadikTizenketto = [25,26,27,28,29,30,31,32,33,34,35,36];
+const feketek = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35];
+const pirosak = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
+const elsoOszlop = [1,4,7,10,13,16,19,22,25,28,31,34];
+const masodikOszlop = [2,5,8,11,14,17,20,23,26,29,32,35];
+const harmadikOszlop = [3,6,9,12,15,18,21,24,27,30,33,36];
+const parosok = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36];
+const paratlanok = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35];
 function betNumber(num) {
     if (allMoney>= bet) {
-        if (bets[num.innerText] >= bet*35) {
+        if (bets[num.innerText] > 0) {
             bets[num.innerText] += bet*35;
         }else{
             bets[num.innerText] = bet*35;
     
         }
         allMoney-=bet;
+        oneRound+=bet;
         console.log(bets);
         
     }
@@ -33,6 +42,7 @@ function betDozen(dozen) {
                 }
             }
             allMoney-=bet;
+            oneRound+=bet;
         }
         else if (dozen.innerText == '2nd 12') {
             for (let index = 0; index < masodikTizenketto.length; index++) {
@@ -45,6 +55,7 @@ function betDozen(dozen) {
                 }
             }
             allMoney-=bet;
+            oneRound+=bet;
         }
         else{
             for (let index = 0; index < harmadikTizenketto.length; index++) {
@@ -57,6 +68,7 @@ function betDozen(dozen) {
                 }
             }
             allMoney-=bet;
+            oneRound+=bet;
         }
     }
 }
@@ -72,6 +84,7 @@ function betFiftyPercent(field) {
                 }
             }
             allMoney-=bet;
+            oneRound+=bet;
         }
         else if(field.innerText = '19 to 36'){
             for (let szam = 19; szam < 37; szam++) {
@@ -83,6 +96,7 @@ function betFiftyPercent(field) {
                 }
             }
             allMoney-=bet;
+            oneRound+=bet;
         }
         else if(field.innerText = 'EVEN'){
             for (let index = 0; index < parosok.length; index++) {
@@ -95,6 +109,7 @@ function betFiftyPercent(field) {
                 
             }
             allMoney-=bet;
+            oneRound+=bet;
         }
         else if(field.innerText = 'ODD'){
             for (let index = 0; index < paratlanok.length; index++) {
@@ -106,6 +121,7 @@ function betFiftyPercent(field) {
                 }
             }
             allMoney-=bet;
+            oneRound+=bet;
         }
         else if(field.innerText='RED'){
             for (let index = 0; index < pirosak.length; index++) {
@@ -117,6 +133,7 @@ function betFiftyPercent(field) {
                 }
             }
             allMoney-=bet;
+            oneRound+=bet;
         }
         else if(field.innerText='BLACK'){
             for (let index = 0; index < feketek.length; index++) {
@@ -128,8 +145,10 @@ function betFiftyPercent(field) {
                 }
             }
             allMoney-=bet;
+            oneRound+=bet;
         }
-}}
+}
+}
 
 
 
@@ -145,12 +164,31 @@ function betColumn(column) {
                 }
             }
             allMoney-=bet;
+            oneRound+=bet;
         }
         else if(column.innerText = '2nd column'){
-
+            for (let index = 0; index < masodikOszlop.length; index++) {
+                const element = masodikOszlop[index];
+                if (bets[element] > 0) {
+                    bets[element] += bet*1
+                } else{
+                    bets[element] = bet*1
+                }
+            }
+            allMoney-=bet;
+            oneRound+=bet;
         }
         else{
-
+            for (let index = 0; index < harmadikOszlop.length; index++) {
+                const element = harmadikOszlop[index];
+                if (bets[element] > 0) {
+                    bets[element] += bet*1
+                } else{
+                    bets[element] = bet*1
+                }
+            }
+            allMoney-=bet;
+            oneRound+=bet;
         }
     }
 }
@@ -162,18 +200,59 @@ function betColumn(column) {
 
 
 
+function hoverColors(field) {
+    Nums = [];
+    if (field.innerText == 'RED') {
+        Nums = document.getElementsByClassName('piros');
+    }
+    else{
+        Nums = document.getElementsByClassName('fekete');
+    }
+    for (let index = 0; index < Nums.length; index++) {
+        const element = Nums[index];
+        element.style.backgroundColor = 'yellow';
+    }
+}
+function noHoverColors(field) {
+    Nums = [];
+    if (field.innerText == 'RED') {
+        Nums = document.getElementsByClassName('piros');
+    }
+    else{
+        Nums = document.getElementsByClassName('fekete');
+    }
+    for (let index = 0; index < Nums.length; index++) {
+        const element = Nums[index];
+        element.style.backgroundColor = '';
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
+function hoverEvenOdd(field) {
+    Nums = [];
+    if (field.innerText == 'ODD') {
+        Nums = document.getElementsByClassName('paros');
+    }
+    else{
+        Nums = document.getElementsByClassName('paratlan');
+    }
+    for (let index = 0; index < Nums.length; index++) {
+        const element = Nums[index];
+        element.style.backgroundColor = 'yellow';
+    }
+}
+function noHoverEvenOdd(field) {
+    Nums = [];
+    if (field.innerText == 'ODD') {
+        Nums = document.getElementsByClassName('paros');
+    }
+    else{
+        Nums = document.getElementsByClassName('paratlan');
+    }
+    for (let index = 0; index < Nums.length; index++) {
+        const element = Nums[index];
+        element.style.backgroundColor = '';
+    }
+}
 
 function hoverDozen(dozen) {
     Nums = []
@@ -245,6 +324,9 @@ function hoverColumn(col) {
     }
     else if(col.innerText == '2nd column'){
         Nums = document.getElementsByClassName('masodikOszlop');
+    }
+    else{
+        Nums = document.getElementsByClassName('harmadikOszlop');
     }
     for (let index = 0; index < Nums.length; index++) {
         Nums[index].style.backgroundColor = 'yellow';
